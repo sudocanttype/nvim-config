394a3dafd8578536ccca07ffd24a7b0cd952ff6e
ol <oscarlay789@gmail.com>
2026-02-17 11:43:23 -0600

    dap config

---
+++ lua/dap_config.lua
@@ -0,0 +1,23 @@
+local dap = require('dap')
+dap.configurations.python = {
+  {
+    type = 'python';
+    request = 'launch';
+    name = "Launch file (global python)";
+    program = "${file}";
+    pythonPath = function()
+      return '/usr/bin/python'
+    end;
+  },
+  {
+    type = 'python';
+    request = 'launch';
+    name = "Launch file (virtualenv python)";
+    program = "${file}";
+    pythonPath = function()
+      return '${workspaceFolder}/venv/bin/python3'
+    end;
+  },
+}
+
+
